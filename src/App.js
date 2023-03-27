40e010f928f5fb04120f8d302f2f601385ec073c
AlfonsoDeBartolo <alfo.deba@gmail.com>
2023-03-27 17:03:26 +0200

    React d1- Es

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ function App() {
         My Images:
         
           <ImageComponent src="https://img.freepik.com/premium-photo/cute-easter-bunny-rabbit-inside-cracked-egg-generative-ai_601748-44450.jpg?w=996" alt="Image 1"/>
-          <ImageComponent src="" alt="Image 2"/>
-          <ImageComponent src="" alt="Image 3"/>
+          <ImageComponent src="https://img.freepik.com/free-photo/happy-easter-day-easter-eggs-wooden-background_74952-2987.jpg?size=626&ext=jpg" alt="Image 2"/>
+          <ImageComponent src="https://img.freepik.com/free-photo/top-view-table-full-delicious-food-composition_23-2149141352.jpg?size=626&ext=jpg" alt="Image 3"/>
        
       </header>
     </div>
